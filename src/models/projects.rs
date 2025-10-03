bcdb33b8bd17f7fb5f284bd5fe32b29bc62d7a58
funnyboy-roks <fbr@funnyboyroks.com>
2025-10-03 05:23:03 -0500

    why were these enums?

--- src/models/projects.rs
+++ src/models/projects.rs
@@ -1,52 +1,19 @@
 use serde::{Deserialize, Serialize};
 use std::{fs, io};
-use strum::EnumVariantNames;
-
-#[derive(Debug, Clone, Serialize, Deserialize, EnumVariantNames)]
-pub enum Language {
-    Rust,
-    Java,
-    JavaScript,
-    TypeScript,
-    Svelte,
-    Python,
-    HTML,
-    CSS,
-    #[serde(other)]
-    Other,
-}
-
-#[derive(Debug, Clone, Serialize, Deserialize)]
-pub enum ProjectStatus {
-    /// Project is complete
-    Complete,
-    /// Project is WIP
-    WIP,
-    /// Project will be continuiously updated
-    NeverEnding,
-    /// Project is no longer supported
-    Unsupported,
-    /// Project complete but still maintained
-    Maintained,
-    /// Project archived
-    Archived,
-}
-
-type URL = String;
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct Link {
     pub name: String,
-    pub dest: URL,
+    pub dest: String,
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct Project {
     pub title: String,
     pub description: String,
-    pub languages: Vec<Language>,
-    pub status: ProjectStatus,
-    pub source: URL,
+    pub languages: Vec<String>,
+    pub status: String,
+    pub source: String,
     pub links: Vec<Link>,
     pub tags: Vec<String>,
 }
